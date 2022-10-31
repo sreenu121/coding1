116d28b11bac67d34d4d6ac5573adde0b921fc06
sreenivasulu atthipati <101385784+sreenu121@users.noreply.github.com>
2022-10-31 23:49:15 +0530

    this for adding and avg
    input taken from user and performing the adding function and calculate the sum of avg

---
+++ ADD1.java
@@ -0,0 +1,20 @@
+import java.util.*;
+class ADd1
+{
+	public static void main(String...args)
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.println("ENTER THE FIST NUMBER");
+		 int num1 = sc.nextInt();
+		 System.out.println("ENTER THE SECOND NUMBER");
+		 int num2 = sc.nextInt();
+		 int sum;
+		 sum =num1+num2;
+		 System.out.println("sumoff of the two numbers"+sum);
+		double avg;
+         avg = sum/2;
+         System.out.println("average of the sum is "+avg);
+	}
+}	
+		
+		
